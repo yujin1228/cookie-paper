f9177483f2c1171dd0209a710005ce104cc54502
yujin1228 <starng11@naver.com>
2023-10-17 00:49:51 +0900

    git 대소문자 구분 설정

---
+++ src/atoms/loginState.js
@@ -0,0 +1,13 @@
+import { atom } from "recoil";
+
+export const LoginState = atom({ 
+    key:'loginstate',
+    default: false
+});
+
+export const UserInfo = atom({
+    key:'user-infomaion',
+    default: {
+        
+    }
+})
