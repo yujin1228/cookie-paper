640da573aaec19dab00d2fe8921793f6a1011a8f
yujin1228 <starng11@naver.com>
2023-10-17 00:45:36 +0900

    not used 삭제

--- src/atoms/LoginState.js
+++ src/atoms/LoginState.js
@@ -1,12 +1,8 @@
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
-
-const { persistAtom } = recoilPersist();
 
 export const LoginState = atom({ 
     key:'loginstate',
-    default: false,
-    //effects_UNSTABLE: [persistAtom]
+    default: false
 });
 
 export const UserInfo = atom({
--- src/atoms/MsgOpenTimer.js
+++ src/atoms/MsgOpenTimer.js
@@ -1,14 +1,11 @@
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
 
-const { persistAtom } = recoilPersist();
 //메시지공개일(2023-11-10) 됐는지 여부
 //2023-11-10 자정 이전 -> false
 //2023-11-10 자정 이후 -> true
 export const MsgOpen = atom({ 
     key:'messageOpen',
-    default: false,
-    //effects_UNSTABLE: [persistAtom]
+    default: false
 });
 
 export const day = atom({
